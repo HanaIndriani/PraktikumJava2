aa3792abc6835f77fb71d87e4376d34bf4a2d726
HanaIndriani <indrianihana30@gmail.com>
2020-05-01 22:15:03 -0700

    Membuat Class Buku

---
+++ src/Buku.java
@@ -0,0 +1,21 @@
+public class Buku {
+    //atribute
+    String judul;
+    String pengarang;
+    String penerbit;
+    int tahun;
+    double harga;
+
+    //behavior
+    void cetakInfo(){
+        System.out.println("Judul Buku : " + judul);
+        System.out.println("Pengarang : "+ pengarang);
+        System.out.println("Penerbit : "+ penerbit);
+        System.out.println("Tahun Terbit : "+ tahun);
+        System.out.println("Harga : "+ harga);
+    }
+
+    void daftarbukuyangpalingdigemari(){
+
+    }
+}
